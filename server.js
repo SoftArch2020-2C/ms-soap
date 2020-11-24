d22352c80392965d5d35917401ba4dff4ff275de
Camilo Sepulveda <csepulvedaz@unal.edu.co>
2020-11-23 22:13:21 -0500

    SIUUUUUUU

--- server.js
+++ server.js
@@ -1,74 +1,64 @@
-
-
-var soap = require('soap');
-var bodyParser = require('body-parser');
-var express = require('express');
-var axios = require('axios');
-const cors = require('cors');
+var soap = require("soap");
+var bodyParser = require("body-parser");
+var express = require("express");
+var axios = require("axios");
+const cors = require("cors");
 const path = require("path");
-var http = require('http');
-var connect = require('connect');
-var logger = require('morgan');
+var http = require("http");
+var connect = require("connect");
+var logger = require("morgan");
 
-var api = require('./api');
+var api = require("./api");
 
 const URL = "https://gateway.froid.ml/graphql"; //proxy url
 
 var service = {
     ws: {
         questionList: {
-            questions : async function(args) {
-                var id_usuario = args.id_usuario
-                const questions = await axios.post(URL, {
-                query: `
-                  query {
-                    listMaterias(id_usuario : "${id_usuario}"){
-                        id
-                        id_usuario
-                        id_semestre
-                        id_tipologia
-                        nombre
-                        prerequisitos
-                        creditos
-                        nota
-                    }
-                    }
-                    `
-                  })
-                if(questions &&  questions.data &&  questions.data.data &&  questions.data.data.listMaterias){
+            questions: async function (args) {
+                return await axios
+                    .post(URL, {
+                        query: `
+                            query {
+                                listMaterias(id_usuario : ${args.id_usuario}){
+                                    id
+                                    id_usuario
+                                    id_semestre
+                                    id_tipologia
+                                    nombre
+                                    prerequisitos
+                                    creditos
+                                    nota
+                                }
+                            }
+                        `,
+                    })
+                    .then((val) => {
+                        const asignature = val.data.data.listMaterias[0];
 
-                    const asignature = questions.data.data.listMaterias[0];
-
-                    console.log("entre1");
-                
-                    return {    
-                                res : "Random question from: "+id_usuario,
-                                id : asignature.id,
-                                id_usuario : asignature.id_usuario,
-                                id_semestre : asignature.id_semestre,
-                                id_tipologia : asignature.id_tipologia,
-                                nombre : asignature.nombre,
-                                prerequisitos : asignature.prerequisitos,
-                                creditos : asignature.creditos,
-                                nota : asignature.nota};
-                }
-                else{
-                  if(questions.data.data.listMaterias.ERROR){
-                      console.log("entre1");
-                    return {res: questions.data.data.listMaterias.ERROR}
-                  }
-
-                  console.log("entre1");
-                  return {res: "ERROR"};
+                        return {
+                            id: asignature.id,
+                            id_usuario: asignature.id_usuario,
+                            id_semestre: asignature.id_semestre,
+                            id_tipologia: asignature.id_tipologia,
+                            nombre: asignature.nombre,
+                            prerequisitos: asignature.prerequisitos,
+                            creditos: asignature.creditos,
+                            nota: asignature.nota,
+                        };
+                    })
+                    .catch((e) => {
+                        return { res: "ERROR" };
+                    });
+                {
                 }
             },
-        }
-    }
-}
+        },
+    },
+};
 
-
- async function test(){
-     var query="";
+async function test() {
+    var query = "";
     const questions = await axios.post(URL, {
         query: `
             query {
@@ -83,38 +73,44 @@ var service = {
                 nota
             }
             }
-            `
-        })
+            `,
+    });
 
-    if(questions &&  questions.data &&  questions.data.data &&  questions.data.data.listMaterias){
+    if (
+        questions &&
+        questions.data &&
+        questions.data.data &&
+        questions.data.data.listMaterias
+    ) {
+        const asignature = questions.data.data.listMaterias[0];
 
-       const asignature = questions.data.data.listMaterias[0];
+        console.log({
+            id: asignature.id,
+            id_usuario: asignature.id_usuario,
+            id_semestre: asignature.id_semestre,
+            id_tipologia: asignature.id_tipologia,
+            nombre: asignature.nombre,
+            prerequisitos: asignature.prerequisitos,
+            creditos: asignature.creditos,
+            nota: asignature.nota,
+        });
 
-       console.log({    id : asignature.id,
-                                id_usuario : asignature.id_usuario,
-                                id_semestre : asignature.id_semestre,
-                                id_tipologia : asignature.id_tipologia,
-                                nombre : asignature.nombre,
-                                prerequisitos : asignature.prerequisitos,
-                                creditos : asignature.creditos,
-                                nota : asignature.nota});
-                
-                    return {    id : asignature.id,
-                                id_usuario : asignature.id_usuario,
-                                id_semestre : asignature.id_semestre,
-                                id_tipologia : asignature.id_tipologia,
-                                nombre : asignature.nombre,
-                                prerequisitos : asignature.prerequisitos,
-                                creditos : asignature.creditos,
-                                nota : asignature.nota};
+        return {
+            id: asignature.id,
+            id_usuario: asignature.id_usuario,
+            id_semestre: asignature.id_semestre,
+            id_tipologia: asignature.id_tipologia,
+            nombre: asignature.nombre,
+            prerequisitos: asignature.prerequisitos,
+            creditos: asignature.creditos,
+            nota: asignature.nota,
+        };
     }
+}
 
-    
- }
+var xml = require("fs").readFileSync("service.wsdl", "utf8");
 
-    var xml = require('fs').readFileSync('service.wsdl', 'utf8');
-
-    /*
+/*
     var app2 = connect()
 
         .use(logger())        
@@ -128,38 +124,43 @@ var service = {
 
     });*/
 
+var app = express();
 
-    var app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin: *");
+    next();
+});
 
+const corsOptions = {
+    origin: "*",
+    //origin: 'http://127.0.0.1:3000',
+};
+app.use(express.json({ limit: "10mb" }));
+//To avoid CORS errors
+app.use(cors(corsOptions));
 
-    app.use(function(req,res,next){
-        res.header('Access-Control-Allow-Origin: *');
-        next();
+//Use public dirname to serve static files
+app.use(express.static(__dirname + "/public"));
+//app.use('/uploads/drivers',express.static(path.join(__dirname, 'public/uploads/drivers')));
+
+//body parser middleware are supported (optional)
+app.use(
+    bodyParser.raw({
+        type: function () {
+            return true;
+        },
+        limit: "5mb",
     })
-
-    const corsOptions = {
-        origin: '*',
-        //origin: 'http://127.0.0.1:3000',
-    };
-    app.use(express.json({limit: '10mb'}));
-    //To avoid CORS errors
-    app.use(cors(corsOptions));
-
-    //Use public dirname to serve static files
-    app.use(express.static(__dirname + '/public'));
-    //app.use('/uploads/drivers',express.static(path.join(__dirname, 'public/uploads/drivers')));
-
-    //body parser middleware are supported (optional)
-    app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
-    app.listen(3000, function(){
-        //Note: /wsdl route will be handled by soap module
-        //and all other routes & middleware will continue to work
-        soap.listen(app, '/asignatures', service, xml, function(){
-            console.log('server initialized');
-        });
+);
+app.listen(3000, function () {
+    //Note: /wsdl route will be handled by soap module
+    //and all other routes & middleware will continue to work
+    soap.listen(app, "/asignatures", service, xml, function () {
+        console.log("server initialized");
     });
+});
 
-    /*
+/*
 
    
     var server = http.createServer(app2);
@@ -198,4 +199,3 @@ var service = {
         // Actions on ready
         console.log('Server: Server is running');
     });*/
-
